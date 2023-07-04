48fd7b1a92b000dc9d896a2acdf264523fd7742e
eabre <68857280+eabre@users.noreply.github.com>
2023-07-04 03:21:52 +0300

    Update firebase.js

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhK6m4sEf0EZMWXhTsKxEZwAH3eXJD_uM",
-  authDomain: "buzzverse-99c91.firebaseapp.com",
-  projectId: "buzzverse-99c91",
-  storageBucket: "buzzverse-99c91.appspot.com",
-  messagingSenderId: "477884519550",
-  appId: "1:477884519550:web:9ca32881d47e86cffcb42f",
-  measurementId: "G-SSG7K4J3M2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
